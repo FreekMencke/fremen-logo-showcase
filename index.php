cde8851cbbec9e4520cecc4fa1c361febed218a1
Freek Mencke <freek_mencke@outlook.com>
2020-05-18 15:14:08 +0200

    Update index.php

--- index.php
+++ index.php
@@ -7,7 +7,7 @@ Description: Gutenberg block that adds a Tiny Slider to showcase logos/images.
 Author: Freek Mencke
 Author URI: https://github.com/FreekMencke
 
-Version: 1.0.2
+Version: 1.0.3
 */
 
 function fremen_logo_showcase_register_block() {
@@ -21,12 +21,17 @@ function fremen_logo_showcase_register_block() {
         $asset_file['dependencies'],
         $asset_file['version']
     );
+    
+    wp_register_script('slider-script', plugins_url('build/slider.js', __FILE__));
+    wp_register_style('slider-style', plugins_url('build/tiny-slider.css', __FILE__));
+    wp_register_style('slider-style-custom', plugins_url('build/custom.css', __FILE__));
  
-    register_block_type('fremen/logo-showcase', array('editor_script' => 'fremen/logo-showcase'));
+    register_block_type('fremen/logo-showcase', array(
+        'editor_script' => 'fremen/logo-showcase',
+        'script' => 'slider-script',
+        'style' => array('slider-style', 'slider-style-custom'),
+    ));
 
-    wp_enqueue_script('slider-script', plugins_url('build/slider.js', __FILE__));
-    wp_enqueue_style('slider-style', plugins_url('build/tiny-slider.css', __FILE__));
-    wp_enqueue_style('slider-style-custom', plugins_url('build/custom.css', __FILE__));
 }
 
 add_action( 'init', 'fremen_logo_showcase_register_block' );
