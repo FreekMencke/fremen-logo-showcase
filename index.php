5536f79aa2e2379d43007df16a9489d8255a208c
Freek Mencke <freek_mencke@outlook.com>
2020-06-04 20:45:34 +0200

    add version to css requests

--- index.php
+++ index.php
@@ -15,16 +15,11 @@ function fremen_logo_showcase_register_block() {
     // automatically load dependencies and version
     $asset_file = include( plugin_dir_path( __FILE__ ) . 'build/index.asset.php');
  
-    wp_register_script(
-        'fremen/logo-showcase',
-        plugins_url( 'build/index.js', __FILE__ ),
-        $asset_file['dependencies'],
-        $asset_file['version']
-    );
+    wp_register_script('fremen/logo-showcase', plugins_url( 'build/index.js', __FILE__ ), $asset_file['dependencies'], $asset_file['version']);
     
-    wp_register_script('slider-script', plugins_url('build/slider.js', __FILE__));
-    wp_register_style('slider-style', plugins_url('build/tiny-slider.css', __FILE__));
-    wp_register_style('slider-style-custom', plugins_url('build/custom.css', __FILE__));
+    wp_register_script('slider-script', plugins_url('build/slider.js', __FILE__), array(), $asset_file['version']);
+    wp_register_style('slider-style', plugins_url('build/tiny-slider.css', __FILE__), array(), $asset_file['version']);
+    wp_register_style('slider-style-custom', plugins_url('build/custom.css', __FILE__), array(), $asset_file['version']);
  
     register_block_type('fremen/logo-showcase', array(
         'editor_script' => 'fremen/logo-showcase',
